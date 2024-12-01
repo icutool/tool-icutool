896d1b9fdafa343a93c7cca0e4e1d65d7a4967f6
PlusBoy <no1plus88@gmail.com>
2024-12-01 12:11:57 +0800

    备份代码

--- src/components/cronGenerate/CronGenerate.vue
+++ src/components/cronGenerate/CronGenerate.vue
@@ -1,88 +1,153 @@
 <template>
   <div id="app">
-    <h1>Cron 表达式生成器</h1>
-
     <!-- CronForm Component -->
-    <div class="cron-form">
-      <h2>选择 Cron 表达式的时间规则</h2>
-      <el-tabs v-model="activeTab" type="card">
-        <!-- Minute Selection -->
-        <el-tab-pane label="分钟" name="minute">
-          <div>
+    <div class="container">
+      <div class="tab">
+        <h2>选择 Cron 表达式的时间规则</h2>
+        <el-tabs v-model="activeTab" value='second' type="card">
+          <!-- second Selection -->
+          <el-tab-pane label="秒钟" name="second">
             <div>
-              <label class="left-align">
-                <input type="radio" v-model="minuteMode" value="*" /> 每秒执行 (允许的通配符: *, -, /)
-              </label>
-            </div>
-            <div>
-              <label class="left-align">
-                <input type="radio" v-model="minuteMode" value="range" /> 周期 从
-                <input type="number" v-model.number="minuteRangeStart" min="0"
-                  :max="unitRange" />
-                -到
-                <input type="number" v-model.number="minuteRangeEnd" min="0"
-                  :max="unitRange" />
-                秒
-              </label>
-            </div>
-            <div>
-              <label class="left-align">
-                <input type="radio" v-model="minuteMode" value="step" /> 从
-                <input type="number" v-model.number="minuteStepStart" min="0"
-                  :max="unitRange" />
-                秒开始, 每
-                <input type="number" v-model.number="minuteStepInterval" min="1"
-                  :max="unitRange" />
-                秒执行一次
-              </label>
-            </div>
+              <div>
+                <label class="left-align">
+                  <input type="radio" v-model="secondMode" value='every' @click="() => cancleSelectMode('every')" />
+                  每秒执行
+                </label>
+              </div>
 
-            <div>
-              <label class="left-align">
-                <input type="radio" v-model="minuteMode" value="numbers" /> 指定
-                <div class="numbers">
-                  <div v-for="(row, index) in numberRows" :key="index" class="row">
-                    <label v-for="number in row" :key="number" class="number">
-                      <input type="checkbox" :value="number" v-model="selectedNumbers" />
-                      {{ number }}
-                    </label>
+              <div>
+                <label class="left-align">
+                  <input type="radio" v-model="secondMode" value="range" @click="() => cancleSelectMode('range')" />
+                  周期性执行 从
+                  <div style="display: inline-block;">
+                    <input type="number" v-model.number="minuteRangeStart" @input="() => updateMinuteValue('range')"
+                      @focus="() => updateMinuteValue('range')" min="0" :max="unitRange" placeholder="起始秒" />
+                    到
+                    <input type="number" v-model.number="minuteRangeEnd" @input="() => updateMinuteValue('range')"
+                      @focus="() => updateMinuteValue('range')" min="0" :max="unitRange" placeholder="结束秒" />
+                    秒 (每秒执行)
                   </div>
-                </div>
-              </label>
+                </label>
+              </div>
+
+              <div>
+                <label class="left-align">
+                  <input type="radio" v-model="secondMode" value="step" @click="() => cancleSelectMode('step')" /> 从
+                  <div style="display: inline-block;">
+                    <input type="number" v-model.number="minuteStepStart" @input="() => updateMinuteValue('step')"
+                      @focus="() => updateMinuteValue('step')" min="0" :max="unitRange" />
+                    秒开始, 每
+                    <input type="number" v-model.number="minuteStepInterval" @input="() => updateMinuteValue('step')"
+                      @focus="() => updateMinuteValue('step')" min="1" :max="unitRange" />
+                    秒执行一次
+                  </div>
+                </label>
+              </div>
+
+              <div>
+                <label class="left-align">
+                  <!-- <input type="radio" v-model="secondMode" value="custom" @click="() => cancleSelectMode('custom')" /> -->
+                  <input type="radio" v-model="secondMode" value="custom" />
+                  指定秒数(可多选)
+                  <div class="numbers">
+                    <div v-for="(row, index) in numberRows" :key="index" class="row">
+                      <label v-for="number in row" :key="number" class="number">
+                        <input type="checkbox" :value="number" v-model="selectedNumbers"  @change="updateMinuteValue('custom')" />
+                        {{ number }}
+                      </label>
+                    </div>
+                  </div>
+                </label>
+              </div>
             </div>
-          </div>
-        </el-tab-pane>
+          </el-tab-pane>
 
-        <!-- Hour Selection -->
-        <el-tab-pane label="小时" name="hour">
-          <!-- <cron-unit v-model="hour" :unitRange="23" /> -->
-        </el-tab-pane>
+          <!-- Minute Selection -->
+          <el-tab-pane label="分钟" name="minute">
+            <div>
+              <div>
+                <label class="left-align">
+                  <input type="radio" v-model="minuteMode" value='every' @click="cancleSelectMode" /> 每秒执行
+                </label>
+              </div>
 
-        <!-- Day Selection -->
-        <el-tab-pane label="日期" name="day">
-          <!-- <cron-unit v-model="day" :unitRange="31" /> -->
-        </el-tab-pane>
+              <div>
+                <label class="left-align">
+                  <input type="radio" v-model="minuteMode" value="range" @click="cancleSelectMode" />
+                  周期性执行 从
+                  <div style="display: inline-block;">
+                    <input type="number" v-model.number="minuteRangeStart" @input="() => updateMinuteValue('range')"
+                      @focus="() => updateMinuteValue('range')" min="0" :max="unitRange" placeholder="起始秒" />
+                    到
+                    <input type="number" v-model.number="minuteRangeEnd" @input="() => updateMinuteValue('range')"
+                      @focus="() => updateMinuteValue('range')" min="0" :max="unitRange" placeholder="结束秒" />
+                    秒 (每秒执行)
+                  </div>
+                </label>
+              </div>
 
-        <!-- Month Selection -->
-        <el-tab-pane label="月份" name="month">
-          <!-- <cron-unit v-model="month" :unitRange="12" /> -->
-        </el-tab-pane>
+              <div>
+                <label class="left-align">
+                  <input type="radio" v-model="minuteMode" value="step" @click="cancleSelectMode" /> 从
+                  <div style="display: inline-block;">
+                    <input type="number" v-model.number="minuteStepStart" @input="() => updateMinuteValue('step')"
+                      @focus="() => updateMinuteValue('step')" min="0" :max="unitRange" />
+                    秒开始, 每
+                    <input type="number" v-model.number="minuteStepInterval" @input="() => updateMinuteValue('step')"
+                      @focus="() => updateMinuteValue('step')" min="1" :max="unitRange" />
+                    秒执行一次
+                  </div>
+                </label>
+              </div>
 
-        <!-- Day of Week Selection -->
-        <el-tab-pane label="星期" name="weekday">
-          <!-- <cron-unit v-model="weekday" :unitRange="7" :customLabels="['周日', '周一', '周二', '周三', '周四', '周五', '周六']" /> -->
-        </el-tab-pane>
-      </el-tabs>
+              <div>
+                <label class="left-align">
+                  <input type="radio" v-model="minuteMode" value="custom" @click="cancleSelectMode" /> 指定秒数(可多选)
+                  <div class="numbers">
+                    <div v-for="(row, index) in numberRows" :key="index" class="row">
+                      <label v-for="number in row" :key="number" class="number" >
+                        <input type="checkbox" :value="number" v-model="selectedNumbers" @click="updateMinuteValue('custom')" />
+                        {{ number }}
+                      </label>
+                    </div>
+                  </div>
+                </label>
+              </div>
+            </div>
+          </el-tab-pane>
+
+          <!-- Hour Selection -->
+          <el-tab-pane label="小时" name="hour">
+            <!-- <cron-unit v-model="hour" :unitRange="23" /> -->
+          </el-tab-pane>
+
+          <!-- Day Selection -->
+          <el-tab-pane label="日期" name="day">
+            <!-- <cron-unit v-model="day" :unitRange="31" /> -->
+          </el-tab-pane>
+
+          <!-- Month Selection -->
+          <el-tab-pane label="月份" name="month">
+            <!-- <cron-unit v-model="month" :unitRange="12" /> -->
+          </el-tab-pane>
+
+          <!-- Day of Week Selection -->
+          <el-tab-pane label="星期" name="weekday">
+            <!-- <cron-unit v-model="weekday" :unitRange="7" :customLabels="['周日', '周一', '周二', '周三', '周四', '周五', '周六']" /> -->
+          </el-tab-pane>
+        </el-tabs>
+      </div>
       <div class="input-group">
         <button @click="generateCronExpression">生成 Cron 表达式</button>
       </div>
+      <!-- CronResult Component -->
+      <div class="cron-result">
+        <h2>生成的 Cron 表达式</h2>
+        <p>{{ cronExpression }}</p>
+      </div>
     </div>
 
-    <!-- CronResult Component -->
-    <div class="cron-result">
-      <h2>生成的 Cron 表达式</h2>
-      <p>{{ cronExpression }}</p>
-    </div>
+
   </div>
 </template>
 
@@ -92,6 +157,7 @@ export default {
   data() {
     return {
       activeTab: 'minute',
+      secend: '*',
       minute: '*',
       hour: '*',
       day: '*',
@@ -99,7 +165,10 @@ export default {
       weekday: '*',
       cronExpression: '',
       unitRange: 59,
-      minuteMode: '*',
+
+      secondMode: 'every',
+      minuteMode: 'every',
+
       minuteRangeStart: 0,
       minuteRangeEnd: 59,
       minuteStepStart: 0,
@@ -117,8 +186,46 @@ export default {
   },
   methods: {
     generateCronExpression() {
-      const cron = `${this.minute} ${this.hour} ${this.day} ${this.month} ${this.weekday}`;
+      const cron = `${this.secend} ${this.minute} ${this.hour} ${this.day} ${this.month} ${this.weekday}`;
       this.cronExpression = cron;
+    },
+    cancleSelectMode(modeVlue) {
+      console.log('cancleSelectMode');
+      this.selectedNumbers = [];
+      this.updateMinuteValue(modeVlue);
+      console.log(this.activeTab);
+    },
+    updateMinuteValue(modeVlue) {
+      this.$nextTick(() => {
+        console.log(this.selectedNumbers);
+        console.log(modeVlue);
+      });
+      
+      // this.$nextTick(() => {
+      //   // 动态组合值并赋值给 minuteMode
+      // if (this.activeTab == 'second' || this.activeTab == 'minute') {
+      //     var value;
+      //     if (modeVlue == 'range') {
+      //       value = `${this.minuteRangeStart}-${this.minuteRangeEnd}`;
+      //     } else if (modeVlue == 'step') {
+      //       value = `${this.minuteStepStart}/${this.minuteStepInterval}`;
+      //     } else if (modeVlue == 'custom') {
+      //       console.log(this.selectedNumbers);
+      //       value = this.selectedNumbers.join(',');
+      //     }
+      //     if (this.activeTab == 'second') {
+      //       this.secend = value;
+      //     } else if (this.activeTab == 'minute') {
+      //       this.minute = value;
+      //     }
+      //   }
+      //   if (this.minuteMode != modeVlue) {
+      //     this.minuteMode = modeVlue;
+      //     console.log(this.minuteMode);
+      //   }
+      // });
+      
+
     },
   },
 };
@@ -126,38 +233,83 @@ export default {
 
 <style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  margin-top: 60px;
+  font-family: 'Arial', sans-serif;
+  color: #706f6f;
+  padding: 20px;
+  /* 设置内边距 */
+  min-height: 80vh;
+  /* 设置最小高度 */
+  display: flex;
+  /* 使用 flex 布局 */
+  justify-content: center;
+  /* 水平居中 */
 }
 
-.cron-form {
-  max-width: 400px;
-  margin: 0 auto;
-  padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 8px;
+.container {
+  width: 100%;
+  max-width: 600px;
+  background: #fff;
+  padding: 30px;
+  border-radius: 10px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  height: 70vh;
+  /* 设置总高度为视口高度 */
+}
+
+.tab {
+  flex: 5;
+  /* 70% 高度 */
+}
+
+.input-group {
+  flex: 1;
+  /* 10% 高度 */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+}
+
+.cron-result {
+  flex: 2;
+  /* 20% 高度 */
+  background: #f1f1f1;
+  border: 1px solid #ddd;
+  padding: 10px;
+  box-sizing: border-box;
+  overflow-y: auto;
+  /* 如果内容超出可滚动 */
 }
 
 .left-align {
-    text-align: left;
-    display: block; /* 使其成为块级元素，确保左对齐 */
+  text-align: left;
+  margin-top: 1vh;
+  display: block;
+  /* 使其成为块级元素，确保左对齐 */
 }
 
 .numbers {
   display: flex;
   flex-direction: column;
+  margin: 0 20px;
+  /* 距离左右边框10px */
 }
 
 .row {
   display: flex;
-  flex-wrap: wrap;
-  width: 100%;
+  flex-wrap: nowrap;
+  justify-content: space-between;
 }
 
 .number {
-  width: 10%; /* 设置每个数字占据 10% 宽度 */
-  box-sizing: border-box; /* 确保宽度包括内边距和边框 */
+  width: 50%;
+  /* 设置每个数字占据 50% 宽度 */
+  box-sizing: border-box;
+  /* 确保宽度包括内边距和边框 */
   text-align: center;
+  display: flex;
 }
 </style>
